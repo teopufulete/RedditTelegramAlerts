01a44f507c7ead9459050d8ad644a6771ce54c7e
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2021-04-03 13:45:43 +0300

    initial commit

---
+++ reddit2telegram.py
@@ -0,0 +1,50 @@
+from telegram.ext import Updater, CommandHandler
+import praw
+import logging
+
+
+with open("./info.txt",'r') as f:
+    credentials = f.read().split(',')
+
+client_id, client_secret , reddit_username, reddit_password = [credentials[i] for i in (0,1,2,3)]
+
+
+class RedditAlerts:
+    def __init__(self, client_id, client_secret, reddit_username, reddit_password):
+         self.client_id = client_id
+         self.client_secret = client_secret
+         self.reddit_username = reddit_username
+         self.reddit_password = reddit_password
+         self.is_logged_in = False
+     
+
+    def login(self):
+        self.reddit = praw.Reddit(user_agent="telegram keeb watcher",
+                        client_id=self.client_id,
+                        client_secret=self.client_secret,
+                        username=self.reddit_username,
+                        password=self.reddit_password)
+        reddit.read_only = True
+
+        self.is_logged_in = True
+        print('Login?', self.is_logged_in)
+
+
+    def search_sub(self, subreddit = '', query = ''):
+        reddit = self.reddit
+        subreddit = reddit.subreddit(subreddit)
+    
+        for submission in subreddit.search(query, time_filter = 'day'):
+            print(submission.title)
+          
+          
+reddit = RedditAlerts(client_id, client_secret, reddit_username, reddit_password)
+reddit.login()
+reddit.search_sub('mechmarket', 'tangerine')
+
+
+class TelegramMessages:
+    def __init__(self, token, telegram_channel):
+        self.token = token 
+        self.telegram_channel = telegram_channel      
+    
